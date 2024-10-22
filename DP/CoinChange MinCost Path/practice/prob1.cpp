7695c9d752f50f03bfc7bf9c4eede6ade58ebff7
Pramothsgp <jarvis.tech.006@mail.com>
2024-10-22 11:43:31 +0530

    Coin Change Practice

---
+++ DP/CoinChange MinCost Path/practice/prob1.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+
+using namespace std;
+
+int coinChange(const vector<int>& coins , const int &target){
+    int n = coins.size();
+    vector<int> dp(target+1,INT_MAX);
+    dp[0] = 0;
+    for(int i=1;i<=target;i++){
+        for(int j=0;j<n;j++){
+            if(coins[j] <= i && dp[i-coins[j]] !=INT_MAX) {
+                dp[i] = min(dp[i],dp[i-coins[j]]+1);
+            }
+        }
+    }
+    return dp[target] == INT_MAX? -1 : dp[target];
+}
+
+int main(){
+    int target ;
+    cin>>target;
+    if(target < 0){
+        cout<<"Invalid input";
+        return 0;
+    }
+    int n;
+    cin>>n;
+    vector<int> coins(n);
+    for(int i=0;i<n;i++){
+        cin>>coins[i];
+        if(coins[i] < 0){
+            cout<<"Invalid input";
+            return 0;
+        }
+    }
+    
+    cout << coinChange(coins , target);
+}
---
+++ DP/CoinChange MinCost Path/practice/prob3.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int minCostPath(vector<vector<int>>& grid){
+    int n = grid.size() , m = grid[0].size();
+    for(int i=1;i<n;i++){
+        grid[i][0] += grid[i-1][0];
+    }
+    for(int i=1;i<m;i++){
+        grid[0][i] += grid[0][i-1];
+    }
+    
+    for(int i=1;i<n;i++){
+        for(int j=1;j<m;j++){
+            grid[i][j] += min({grid[i-1][j-1],grid[i-1][j],grid[i][j-1]});
+        }
+    }
+    return grid[n-1][m-1];
+}
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+    if(n<0 || m<0){
+        cout<<"Invalid input";
+        return 0;
+    }
+    vector<vector<int>> grid(n,vector<int>(m));
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>grid[i][j];
+        }
+    }
+    
+    cout << minCostPath(grid);
+}
